ebd1f609d0f28630610cca53906fa4edf19ebc7d
denneyl2711 <lukedenneymn@gmail.com>
2026-08-02 18:10:04 -0500

    Fix display text coloring.

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -7,7 +7,7 @@ return {
                     'When sold, {C:green}#3# in #4# chance{}',
                     'to spawn a copy of this Joker with',
                     '{X:mult,C:white} X#2# {} Mult and decreased odds',
-                    '{C:inactive} (Currently {X:mult,C:white}X#1#{} Mult){}'
+                    '{C:inactive} (Currently {X:mult,C:white}X#1# {C:inactive} Mult){}'
                 }
             }
         }
